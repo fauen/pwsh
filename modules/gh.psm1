d5acdbb7d42da6999a518331fad5bf6cf97d51dc
fauen <daniel@backman.io>
2023-03-22 06:40:33 +0100

    Added a help section.

--- modules/gh.psm1
+++ modules/gh.psm1
@@ -1,3 +1,23 @@
+<#
+    .SYNOPSIS
+    An alternative way of using some simple git commands.
+
+    .DESCRIPTION
+    The whole point of this was to have PowerShell like git commands instead of using the normal ones.
+    Why? Good learning experience.
+
+    .PARAMETER Read
+    -Read = git status
+
+    .PARAMETER Receive
+    -Receive = git pull
+
+    .PARAMETER Send
+    -Send = git push
+
+    .PARAMETER Write
+    -Write = git add .; git commit; git push
+#>
 function GH {
     param (
         [Parameter(Mandatory=$false)]
