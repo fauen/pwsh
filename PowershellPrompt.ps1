9794f7891efe960be5d13fbf85e52463f3fa3ac6
fauen <daniel@backman.io>
2023-08-03 19:46:28 +0200

    Prompt done.
    Finally have my promp the way I want to. If an error the >>> turns red
    and if command completes successfully it's green.

--- PowershellPrompt.ps1
+++ PowershellPrompt.ps1
@@ -2,5 +2,10 @@ $username = [System.Environment]::UserName
 $machinename = [System.Environment]::MachineName
 
 function prompt {
-   "$username`@$machinename`:$(Get-Location) >>> " 
+   if (!$?) {
+      "$username`@$machinename`:$(Get-Location) `e[31m>>>`e[0m " 
+   }
+   else {
+      "$username`@$machinename`:$(Get-Location) `e[32m>>>`e[0m " 
+   }
 }
