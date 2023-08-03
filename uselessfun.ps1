7de05865e737f77d9a10afae17006351c4a7f73d
fauen <daniel@backman.io>
2023-08-03 20:51:27 +0200

    Added some more API stuff.
    More useless things but it's fun.

--- uselessfun.ps1
+++ uselessfun.ps1
@@ -3,6 +3,9 @@
 $options = @"
 1. Dad joke
 2. Affirmation
+3. Guess age from name
+4. Chuck Norris fact
+5. Country facts
 "@
 
 $options | Out-Host
@@ -17,6 +20,21 @@ switch ($answer) {
     2 {
         $affirmation = (Invoke-RestMethod -Method Get -Uri "https://www.affirmations.dev")
         $affirmation.affirmation
+    }
+    3 {
+        $yourName = Read-Host "Input your name"
+        $guessAge = (Invoke-RestMethod -Method Get -Uri "https://api.agify.io/?name=$yourname")
+        $guessAge
+    }
+    4 {
+        $chuck = (Invoke-RestMethod -Method Get -Uri "https://api.chucknorris.io/jokes/random")
+        $chuck.value
+    }
+    5 {
+        $countryName = Read-Host "Input country"
+        $country = (Invoke-RestMethod -Method Get -Uri "https://restcountries.com/v3.1/name/$countryName")
+        $country
+        Write-Output "For more details use the `$country variable."
     }
     Default {
         Break
