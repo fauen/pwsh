0f4dd2a441fb1bc4459123d9f3813069b27b7500
fauen <daniel@backman.io>
2023-08-03 20:27:09 +0200

    Cleaning.
    Removed almost everything and it's much easier to read and use. It's so
    tiny now that I think I might but it in the normal profile eventually.

--- ImportModules.ps1
+++ ImportModules.ps1
@@ -2,13 +2,3 @@ $modulesPath = Join-Path -Path "$HOME\pwsh\" -ChildPath "modules"
 foreach ($i in Get-ChildItem $modulesPath) {
     Import-Module $i
 }
-#if ($args[0]) {
-#    foreach ($i in Get-ChildItem $args[0]) {
-#        Import-Module $i
-#    }
-#}
-#else {
-#    foreach ($i in Get-ChildItem "$HOME\pwsh\modules") {
-#        Import-Module $i
-#    }
-#}
