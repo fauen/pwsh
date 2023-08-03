31895db4636b733f57859bd47e94834344566400
fauen <daniel@backman.io>
2023-08-03 17:29:23 +0200

    Reverted back it's working now.

--- PowershellPrompt.ps1
+++ PowershellPrompt.ps1
@@ -1,7 +1,7 @@
 $username = [System.Environment]::UserName
 $machinename = [System.Environment]::MachineName
-$location = [System.Environment]::CurrentDirectory
+#$location = [System.Environment]::CurrentDirectory
 
 function prompt {
-   "$username`@$machinename`:$location >>> " 
+   "$username`@$machinename`:$(Get-Location) >>> " 
 }
