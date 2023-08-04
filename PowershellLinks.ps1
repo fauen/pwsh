ed972b96bee8caeacc4c0e1e7ccbac639e813bc8
fauen <daniel@backman.io>
2023-08-04 12:34:38 +0200

    Tweaking.
    Don't know if it works but did some tweaking.

--- PowershellLinks.ps1
+++ PowershellLinks.ps1
@@ -6,17 +6,10 @@ if ($PSVersionTable.Platform -eq "Unix") {
         mkdir $HOME/.config/powershell
         Write-Verbose "Directory has been created."
     }
-#    ln -s $HOME/pwsh/Microsoft.PowerShell_profile.ps1 $HOME/.config/powershell/$1
-#    ln -s $HOME/pwsh/PowershellPrompt.ps1 $HOME/.config/powershell/$1
-#    ln -s $HOME/pwsh/PowershellAliases.ps1 $HOME/.config/powershell/$1
-    New-Item -ItemType SymbolicLink -Path "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1" -Target "$configPath/Microsoft.PowerShell_profile.ps1"
 }
 else {
     Start-Process ms-settings:developers
     $enableDev = Get-Process SystemSettings
     Wait-Process -Id $enableDev.Id
-    New-Item -ItemType SymbolicLink -Path "$PROFILE" -Target "$configPath\Microsoft.PowerShell_profile.ps1"
-    #Start-Process "pwsh" -Verb RunAs -ArgumentList "-file $HOME\pwsh\PowershellLinksWin.ps1"
 }
-
-# On Windows, make sure your machine has enabled developer mode
+New-Item -ItemType SymbolicLink -Path "$PROFILE" -Target "$configPath\Microsoft.PowerShell_profile.ps1"
