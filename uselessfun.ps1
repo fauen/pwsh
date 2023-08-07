e58c37fda6109af22d17128572d59740b3be9971
fauen <daniel@backman.io>
2023-08-07 16:32:00 +0200

    Smaller update.
    Added some clearing so you can see the stuff clearly.

--- uselessfun.ps1
+++ uselessfun.ps1
@@ -1,11 +1,15 @@
 # There is no point in this. Just something I wanted to do.
-
+$validAnswer = $false
+while (!$validAnswer) {
+    
 $options = @"
+
 1. Dad joke
 2. Affirmation
 3. Guess age from name
 4. Chuck Norris fact
 5. Country facts
+0. Exit
 "@
 
 $options | Out-Host
@@ -15,28 +19,39 @@ $answer = Read-Host "Pick what you need today"
 switch ($answer) {
     1 {
         $dadjoke = (Invoke-RestMethod -Method Get -Uri "https://icanhazdadjoke.com" -Headers @{Accept='text/plain'})
-        $dadjoke
+        Clear-Host
+        Write-Host -ForegroundColor Green $dadjoke | Out-Host
     }
     2 {
         $affirmation = (Invoke-RestMethod -Method Get -Uri "https://www.affirmations.dev")
-        $affirmation.affirmation
+        Clear-Host
+        Write-Host -ForegroundColor Magenta $affirmation.affirmation | Out-Host
     }
     3 {
         $yourName = Read-Host "Input your name"
         $guessAge = (Invoke-RestMethod -Method Get -Uri "https://api.agify.io/?name=$yourname")
-        $guessAge
+        Clear-Host
+        $guessAge | Out-Host
     }
     4 {
         $chuck = (Invoke-RestMethod -Method Get -Uri "https://api.chucknorris.io/jokes/random")
-        $chuck.value
+        Clear-Host
+        $chuck.value | Out-Host
     }
     5 {
         $countryName = Read-Host "Input country"
         $country = (Invoke-RestMethod -Method Get -Uri "https://restcountries.com/v3.1/name/$countryName")
-        $country
+        Clear-Host
+        $country | Out-Host
         Write-Output "For more details use the `$country variable."
     }
-    Default {
+    0 {
+        $validAnswer = $true
         Break
     }
+    Default {
+        $validAnswer = $true
+        Break
+    }
+}
 }
