ab663ea1b5570901070c22202df551cb4044a054
fauen <daniel@backman.io>
2023-08-07 14:42:26 +0200

    Converted to functions.
    Made it cleaner by converting it to functions instead of a normal script.

--- base64.ps1
+++ base64.ps1
@@ -1,4 +1,37 @@
-$inputText = Read-Host "Input string"
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($inputText)
-$encode = [Convert]::ToBase64String($bytes)
-$encode
+function ConvertTo-Base64 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$False)]
+        [string]$UserInput
+    )
+    if ($UserInput) {
+        $Bytes = [System.Text.Encoding]::UTF8.GetBytes($UserInput)
+        $Encode = [System.Convert]::ToBase64String($Bytes)
+        Write-Host $Encode
+    }
+    else {
+        $UserInput = Read-Host "Input string"
+        $Bytes = [System.Text.Encoding]::UTF8.GetBytes($UserInput)
+        $Encode = [System.Convert]::ToBase64String($Bytes)
+        Write-Host $Encode
+    }
+    
+}
+
+function ConvertFrom-Base64 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$False)]
+        [string]$UserInput
+    )
+    if ($UserInput) {
+        $Decode = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($UserInput))
+        Write-Host $Decode
+    }
+    else {
+        $UserInput = Read-Host -Prompt "Input base64"
+        $Decode = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($UserInput))
+        Write-Host $Decode
+    }
+    
+}
