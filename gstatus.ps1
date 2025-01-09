253a3adb6c2d1390c6f2fea8a3f4346084b051a9
Daniel Bäckman <daniel@backman.io>
2025-01-09 20:25:57 +0100

    Minor addition and change.
    Just adding a "ForegroundColor" so it's easier to see the directory
    name.

--- gstatus.ps1
+++ gstatus.ps1
@@ -1,5 +1,5 @@
 $children = Get-ChildItem -Attribute Directory
 foreach ($item in $children) {
     Write-Host "Entering $item..." -ForegroundColor Green
-    Set-Location $item; git pull; Set-Location ..
+    Set-Location $item; git status; Set-Location ..
 }
